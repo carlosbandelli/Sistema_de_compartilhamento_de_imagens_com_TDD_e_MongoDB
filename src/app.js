b9a789860175f3c7e18f230def9df50a75b9b4e3
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-08-24 15:55:24 -0300

    Teste sendo aplicados

---
+++ src/app.js
@@ -0,0 +1,51 @@
+let express = require("express")
+let app = express()
+let mongoose = require("mongoose")
+let user = require("./models/User")
+let bcrypt = require("bcrypt")
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+mongoose.connect("mongodb://localhost:27017/guiapics", {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        //console.log("Conectado com o banco")
+    }).catch((err)=> {
+        console.log(err)
+    })
+
+let User = mongoose.model("User",user)
+
+app.get("/",(req, res) => {
+    res.json({})
+})
+
+app.post("/user",async (req,res) => { 
+    
+    if(req.body.name == "" || req.body.email == "" || req.body.password == ""){
+        res.sendStatus(400)
+        return
+    }  
+
+    try {
+        let user = await User.findOne({"email": req.body.email})
+
+        if(user != undefined){
+            res.statusCode = 400
+            res.json({error: "E-mail já cadastrado"})
+            return
+        }
+
+        let password = req.body.password
+        let salt = await bcrypt.genSalt(10)
+        let hash = await bcrypt.hash(password,salt)
+
+        let newUser = new User({name: req.body.name, email: req.body.email, password: hash})
+        await newUser.save()
+        res.json({email: req.body.email})
+    } catch (err) {
+        res.sendStatus(500)
+    }
+})
+
+module.exports = app
---
+++ test/user.test.js
@@ -0,0 +1,61 @@
+let app = require("../src/app")
+let supertest = require("supertest")
+let request = supertest(app)
+
+
+describe("Cadastro de úsuario", () => {
+    test("Deve cadastrar um usuário com sucesso", () => {
+
+        let time = Date.now()
+        let email = `${time}@gmail.com`
+        let user = {name: "Victor", email, password: "123456"}
+
+        return request.post("/user")
+        .send(user)
+        .then(res => {
+            expect(res.statusCode).toEqual(200)
+            expect(res.body.email).toEqual(email)
+        }).catch(err => {
+            fail(err)
+        })
+    })
+
+    test("Deve impedir que um usuário se cadastre com so dados vazios", () => {
+
+        let user = {name: "", email: "", password: ""}
+
+        return request.post("/user")
+        .send(user)
+        .then(res => {
+            expect(res.statusCode).toEqual(400)            
+        }).catch(err => {
+            fail(err)
+        })
+    })
+    
+    test("Deve impedir que um usuário se cadastre com um e-mail repetido", ()=>{
+
+        let time = Date.now()
+        let email = `${time}@gmail.com`
+        let user = {name: "Victor", email, password: "123456"}
+
+        return request.post("/user")
+        .send(user)
+        .then(res => {
+            expect(res.statusCode).toEqual(200)
+            expect(res.body.email).toEqual(email)
+
+                //Segunda requisição
+                return request.post("/user")
+                .send(user)
+                .then(res => {
+                    expect(res.statusCode).toEqual(400)
+                    expect(res.body.error).toEqual("E-mail já cadastrado")
+                }).catch(err => {
+                    fail(err)
+                })
+        }).catch(err => {
+            fail(err)
+        })
+    })
+})
