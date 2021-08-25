fc89c398ba69a9b8399a841c0fdeea274a80a9dc
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-08-24 21:50:55 -0300

    e-mail repetido

--- test/user.test.js
+++ test/user.test.js
@@ -2,6 +2,20 @@ let app = require("../src/app")
 let supertest = require("supertest")
 let request = supertest(app)
 
+let mainUser = {name: "Carlos Bandelli", email: "carlos@bandelli.com", password: "123456"}
+
+beforeAll(() => {
+    return request.post("/user")
+    .send(mainUser)
+    .then(res => {})
+    .catch(err => {console.log(err)})
+})
+
+afterAll(() => {
+    return request.delete(`/user/${mainUser.email}`)
+    .then(res => {})
+    .catch(err => {console.log(err)})
+})
 
 describe("Cadastro de úsuario", () => {
     test("Deve cadastrar um usuário com sucesso", () => {
